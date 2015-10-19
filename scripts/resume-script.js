e90ede1a900c37298d7850f98e5d5d56acb65dcc
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-18 23:33:20 -0700

    Add github logo and more CSS.

---
+++ scripts/resume-script.js
@@ -0,0 +1,14 @@
+$(document).ready( function() {
+    /*$('.dropdown_menu ul').hide();
+    $('.dropdown_menu').click(function() {
+        $('ul').slideToggle('slow');
+    });*/
+
+    $(".pastCareers").accordion({
+        collapsible: true, 
+        active: false,
+        navigation: true,
+        autoHeight: false,
+        heightStyle: "content",
+    });
+});
